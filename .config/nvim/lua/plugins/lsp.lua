f49cd4f6a0f8766f8220d65a7d5a59a3244bb73e
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-07-09 08:20:38 +0700

    feat(nvim): disable lsp when inside a comments

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -41,5 +41,17 @@ return {
         format = require("tailwindcss-colorizer-cmp").formatter,
       }
     end,
+    config = {
+      enabled = function()
+        -- disable completion in comments
+        local context = require("cmp.config.context")
+        -- keep command mode completion enabled when cursor is in a comment
+        if vim.api.nvim_get_mode().mode == "c" then
+          return true
+        else
+          return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+        end
+      end,
+    },
   },
 }
