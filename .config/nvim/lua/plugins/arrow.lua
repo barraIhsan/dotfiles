c88b77b709675442980a95d704134914220a25f5
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-08-27 10:53:07 +0700

    feat(nvim): add `arrow.nvim`
    `arrow.nvim` is a plugin to bookmark file similar to how `harpoon` does.

---
+++ .config/nvim/lua/plugins/arrow.lua
@@ -0,0 +1,8 @@
+return {
+	"otavioschwanck/arrow.nvim",
+	opts = {
+		show_icons = true,
+		leader_key = "<leader>a",
+		seperate_by_branch = true, -- bookmarks will be seperated by git branch
+	},
+}
