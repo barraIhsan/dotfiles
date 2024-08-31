1f037280e50db993c5f9d50c32f2476df2cdcd77
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-08-31 17:16:52 +0700

    feat(nvim): bordered `nvim-cmp` window

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -33,6 +33,9 @@ return {
 					luasnip.lsp_expand(args.body)
 				end,
 			},
+			window = {
+				completion = cmp.config.window.bordered(),
+			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
