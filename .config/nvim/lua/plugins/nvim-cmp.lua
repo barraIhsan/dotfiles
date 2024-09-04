8ce89d4c342827cef7979ee0de7db0d562908e5f
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-09-04 19:46:27 +0700

    chorus(nvim): make the documentation also bordered

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -37,6 +37,7 @@ return {
 			},
 			window = {
 				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
 			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
