49e890a46773a1376b7f699fd57cdba807c9b532
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-09-06 22:32:38 +0700

    fix(nvim): fix markdown-preview build scripts
    See https://github.com/iamcco/markdown-preview.nvim/issues/690

--- .config/nvim/lua/plugins/etcetera.lua
+++ .config/nvim/lua/plugins/etcetera.lua
@@ -47,6 +47,7 @@ return {
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = "markdown",
 		build = function()
+			require("lazy").load({ plugins = { "markdown-preview.nvim" } })
 			vim.fn["mkdp#util#install"]()
 		end,
 	},
