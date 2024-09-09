9bb716bd187f1cfbac9c113c18732d03d96e497b
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-09-09 20:25:42 +0700

    chore(nvim): add `rustfmt` formatter for rust

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -5,6 +5,7 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
+				rust = { "rustfmt" },
 				python = { "isort", "black" },
 				html = { "prettierd" },
 				astro = { "prettierd" },
