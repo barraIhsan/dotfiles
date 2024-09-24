00e2154e6d72ad796a3d3eac2507024a9afae918
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-09-24 19:54:45 +0700

    feat(nvim): apply chezmoi changes when saving

--- dot_config/nvim/lua/core/autocmd.lua
+++ dot_config/nvim/lua/core/autocmd.lua
@@ -5,3 +5,12 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+-- apply chezmoi changes when saving a file
+vim.api.nvim_create_autocmd("BufWritePost", {
+  desc = "Apply chezmoi changes after saving any file in the ~/.local/share/chezmoi directory",
+  pattern = vim.fn.expand("~") .. "/.local/share/chezmoi/*",
+  callback = function()
+    vim.fn.system("chezmoi apply --source-path " .. vim.fn.expand("%"))
+  end,
+})
