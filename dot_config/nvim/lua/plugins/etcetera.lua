915709b1aa3ec02e91dedf940f3b93d9ed6a5098
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-10-16 12:52:08 +0700

    chore(nvim): add treesitter as deps for leetcode

--- dot_config/nvim/lua/plugins/etcetera.lua
+++ dot_config/nvim/lua/plugins/etcetera.lua
@@ -61,6 +61,7 @@ return {
     cmd = "Leet",
     build = ":TSUpdate html",
     dependencies = {
+      "nvim-treesitter/nvim-treesitter",
       "nvim-telescope/telescope.nvim",
       "nvim-lua/plenary.nvim", -- required by telescope
       "MunifTanjim/nui.nvim",
