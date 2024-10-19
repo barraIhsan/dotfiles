2e139caff07dbf393a9b5cab57a7e7727c583f27
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-10-19 10:58:35 +0700

    chore(nvim): unbind telescope incremental select

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -33,15 +33,6 @@ return {
         "c",
         "rust",
       },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
     })
   end,
 }
