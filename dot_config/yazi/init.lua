b2b9573d85e107feac2ab0ae998446b46d6652d3
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-11-29 23:05:00 +0700

    feat(yazi): install some plugin
    namely border, show symlink in status bar, show username and hostname in
    header, show git status on file, and preview audio metadata and cover

---
+++ dot_config/yazi/init.lua
@@ -0,0 +1,29 @@
+-- git status on file
+require("git"):setup()
+
+-- full border
+require("full-border"):setup()
+
+-- show symlink in status bar
+-- https://yazi-rs.github.io/docs/tips/#symlink-in-status
+function Status:name()
+	local h = self._tab.current.hovered
+	if not h then
+		return ui.Line({})
+	end
+
+	local linked = ""
+	if h.link_to ~= nil then
+		linked = " -> " .. tostring(h.link_to)
+	end
+	return ui.Line(" " .. h.name .. linked)
+end
+
+-- show username and hostname in header
+-- https://yazi-rs.github.io/docs/tips#username-hostname-in-header
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ui.Line({})
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
