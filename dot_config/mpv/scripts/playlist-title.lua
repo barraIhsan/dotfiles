21392b72190e0894a7c7ca676c5caae4e14a815e
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-03-06 09:22:17 +0700

    refactor(mpv): use 2 space instead of tab stylue

--- dot_config/mpv/scripts/playlist-title.lua
+++ dot_config/mpv/scripts/playlist-title.lua
@@ -1,21 +1,21 @@
 require("mp")
 
 local function title()
-	local pl_index = mp.get_property("playlist-current-pos")
-	local pl_title = mp.get_property("playlist/" .. pl_index .. "/title")
+  local pl_index = mp.get_property("playlist-current-pos")
+  local pl_title = mp.get_property("playlist/" .. pl_index .. "/title")
 
-	-- Title metadata on a file (e.g. mkv) will
-	-- be prioritized by default on mpv over playlist
-	-- title.
-	--
-	-- But sometimes the title sucks so I made
-	-- a playlist file for that.
-	--
-	-- Force media title to be the playlist title
-	-- only if it have a playlist title
-	if pl_title ~= nil then
-		mp.set_property("force-media-title", pl_title)
-	end
+  -- Title metadata on a file (e.g. mkv) will
+  -- be prioritized by default on mpv over playlist
+  -- title.
+  --
+  -- But sometimes the title sucks so I made
+  -- a playlist file for that.
+  --
+  -- Force media title to be the playlist title
+  -- only if it have a playlist title
+  if pl_title ~= nil then
+    mp.set_property("force-media-title", pl_title)
+  end
 end
 
 mp.register_event("file-loaded", title)
