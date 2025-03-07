644f87ee5c486f46f657292db4444168ce3ede6f
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-03-07 21:06:15 +0700

    style(nvim): make nvim-tree float in the centre

--- dot_config/nvim/lua/plugins/nvim-tree.lua
+++ dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -8,10 +8,29 @@ return {
 
     require("nvim-tree").setup({
       view = {
-        width = 35,
         relativenumber = true,
         number = true,
-        side = "right",
+        float = {
+          enable = true,
+          open_win_config = function()
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+            local window_w = screen_w * 0.8
+            local window_h = screen_h * 0.8
+            local window_w_int = math.floor(window_w)
+            local window_h_int = math.floor(window_h)
+            local center_x = (screen_w - window_w) / 2
+            local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+            return {
+              border = "rounded",
+              relative = "editor",
+              row = center_y,
+              col = center_x,
+              width = window_w_int,
+              height = window_h_int,
+            }
+          end,
+        },
       },
       renderer = {
         indent_markers = {
