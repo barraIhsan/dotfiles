2bcc54abf94c4a6b48ed6e69afb3d7ea722f3475
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-03-07 21:56:16 +0700

    feat(nvim): add some window and tab navigation
    Assume `xx` as `<leader>xx`.
    Well firstly, I renamed "split" into "window" as it is the correct
    name.
    
    And then, change `wx` and `tx` to be `wq` and `tq`. To make it the same
    as the equivalent <C-w> keybind. And for `wx` specifically, it's for
    swap split now.
    
    Then, `Tab` and `Shift+Tab` now go to next/previous tab for easier
    navigation.
    
    Lastly, I add `w` + `hjkl` to move window to far (top/left/right/bottom)

--- dot_config/nvim/lua/core/keymaps.lua
+++ dot_config/nvim/lua/core/keymaps.lua
@@ -8,16 +8,23 @@ local keymap = vim.keymap
 keymap.set("n", "<Esc>", "<cmd>nohl<CR>", { desc = "Clear search highlights" })
 
 -- window management
-keymap.set("n", "<leader>s|", "<C-w>v", { desc = "Split window to the right" })
-keymap.set("n", "<leader>s-", "<C-w>s", { desc = "Split window below" })
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+keymap.set("n", "<leader>w|", "<C-w>v", { desc = "Split window to the right" })
+keymap.set("n", "<leader>w-", "<C-w>s", { desc = "Split window below" })
+keymap.set("n", "<leader>we", "<C-w>=", { desc = "Make window equal size" })
+keymap.set("n", "<leader>wq", "<C-w>q", { desc = "Close current window" })
+keymap.set("n", "<leader>wx", "<C-w>x", { desc = "Swap current window" })
+keymap.set("n", "<leader>wt", "<C-w>T", { desc = "Move window into new tab" })
+
+-- move window
+keymap.set("n", "<leader>wh", "<C-w>H", { desc = "Move window to far right" })
+keymap.set("n", "<leader>wj", "<C-w>J", { desc = "Move window to far bottom" })
+keymap.set("n", "<leader>wk", "<C-w>K", { desc = "Move window to far top" })
+keymap.set("n", "<leader>wl", "<C-w>L", { desc = "Move window to far left" })
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+keymap.set("n", "<leader>tq", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<tab>", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<S-tab>", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 
 -- set the cursor in the middle when doing ctrl-d ctrl-u
 keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
