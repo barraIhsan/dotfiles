d34ab633851c3b7dd4b8c73a8f021a6a60d4ab6d
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-03-14 15:41:38 +0700

    fix(mpv): actually select the best audio codec this time
    Previously, I don't even add point to the best codec, but add point to
    all of them.
    This time, I added a point ONLY to the best codec, and also, in this
    commit, add some comments to address what is the meaning of those
    variable names (e.g. `bcodec`)
    
    Maybe I also need to compare audio with their sample rate. But of course
    lossless file always win, followed by good codec, followed by good
    sample rate.
    So flac > opus 44.1khz > aac 96khz

--- dot_config/mpv/scripts/trackselect.lua
+++ dot_config/mpv/scripts/trackselect.lua
@@ -17,6 +17,7 @@ require("mp")
 -- return the highest score id
 -- if it's a tie, return the first one found
 local function highest(rate)
+  -- r means result
   local rid, rscore = 1, 0
   for id, score in pairs(rate) do
     if score > rscore then
@@ -28,15 +29,19 @@ local function highest(rate)
 end
 
 local function select()
+  -- a and s means audio and s respectively
   local arate = {}
   local srate = {}
 
   -- list audio codec from best
-  local codec = { "flac", "eac3", "opus", "aac", "mp3" }
-  local bcodec = #codec
+  local codec = { flac = 5, eac3 = 4, opus = 3, aac = 2, mp3 = 1 }
+  -- b means best
+  local bcodec = 1
+  local bcodec_tid = 1
 
   local tracklist = mp.get_property_native("track-list")
   for _, track in ipairs(tracklist) do
+    -- t means track
     local tid = track["id"]
 
     -- if there's japan audio
@@ -52,11 +57,12 @@ local function select()
         end
 
         -- select the best codec
-        for i, c in ipairs(codec) do
-          if track["codec"] == c and i < bcodec then
-            bcodec = i
-            arate[tid] = arate[tid] + 1
-          end
+        -- if track["codec"] == c and i < bcodec then
+        --   arate[tid] = arate[tid] + 1
+        -- end
+        if codec[track["codec"]] > bcodec then
+          bcodec = codec[track["codec"]]
+          bcodec_tid = track["id"]
         end
       end
     end
@@ -87,6 +93,9 @@ local function select()
       end
     end
   end
+
+  -- give score to the best acodec
+  arate[bcodec_tid] = arate[bcodec_tid] + 1
 
   -- select audio and sub track
   mp.set_property("aid", highest(arate))
