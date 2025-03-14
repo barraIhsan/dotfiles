c2e74d18600126cd8ebcd1daee7456a96c6b367a
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-03-14 14:43:52 +0700

    feat(mpv): use my own trackselect script
    The po5's script is lacking some feature, so that's why I wrote a simple
    script just to do that. You can find the details on the file's comment

---
+++ dot_config/mpv/scripts/trackselect.lua
@@ -0,0 +1,95 @@
+-- Inspired by po5's trackselect, hence the name
+-- https://github.com/po5/trackselect/blob/master/trackselect.lua
+-- which in turn, is also inspired by siikamiika's dualaudiofix
+-- https://github.com/siikamiika/scripts/blob/master/mpv%20scripts/dualaudiofix.lua
+--
+-- This will select the best japanese audio
+-- stereo if possible, and select the best codec
+-- flac -> eac3 -> opus -> aac -> mp3
+--
+-- Also select the best english subttiles
+-- avoid "sign" subs (sign and song only, for english audio),
+-- with honorifics (e.g. onii-chan, senpai, nee-sama) if possible,
+-- and select ass (advanced substation alpha) subtitle if possible
+
+require("mp")
+
+-- return the highest score id
+-- if it's a tie, return the first one found
+local function highest(rate)
+  local rid, rscore = 1, 0
+  for id, score in pairs(rate) do
+    if score > rscore then
+      rid, rscore = id, score
+    end
+  end
+
+  return rid
+end
+
+local function select()
+  local arate = {}
+  local srate = {}
+
+  -- list audio codec from best
+  local codec = { "flac", "eac3", "opus", "aac", "mp3" }
+  local codecid = #codec
+
+  local tracklist = mp.get_property_native("track-list")
+  for _, track in ipairs(tracklist) do
+    local tid = track["id"]
+
+    -- if there's japan audio
+    -- select the best one
+    if track["type"] == "audio" then
+      if track["lang"] == "ja" or string.match(track["lang"], "jpn?") then
+        -- set initial
+        arate[tid] = 0
+
+        -- no mono audio please
+        if track["demux-channel-count"] < 2 then
+          arate[tid] = arate[tid] - 1
+        end
+
+        -- select the best codec
+        for i, c in ipairs(codec) do
+          if track["codec"] == c and i < codecid then
+            arate[tid] = arate[tid] + 1
+          end
+        end
+      end
+    end
+
+    -- select english sub
+    -- avoid "sign" and "s&s"
+    -- prioritize "honor" on sub "title"
+    -- prioritize ass on sub "codec"
+    if track["type"] == "sub" then
+      if string.match(track["lang"], "en.?") then
+        -- set initial
+        srate[tid] = 0
+        -- title
+        local title = (track["title"] or ""):lower()
+
+        -- avoid "sign" and "s&s"
+        if string.match(title, "sign") or string.match(title, "s&s") then
+          srate[tid] = srate[tid] - 2
+        end
+        -- prioritize "honor" or "honour"
+        if string.match(title, "honou?r") then
+          srate[tid] = srate[tid] + 1
+        end
+        -- prioritize ass subtitle
+        if track["codec"] == "ass" then
+          srate[tid] = srate[tid] + 1
+        end
+      end
+    end
+  end
+
+  -- select audio and sub track
+  mp.set_property("aid", highest(arate))
+  mp.set_property("sid", highest(srate))
+end
+
+mp.register_event("file-loaded", select)
