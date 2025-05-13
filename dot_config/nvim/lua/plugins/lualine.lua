2fe81650162e07639862e56e5815c1ea8fbae021
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-05-13 13:30:54 +0700

    feat(nvim,lualine): use abbr for mode and add filetype on the right

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -25,6 +25,9 @@ return {
       ["MORE"] = "M",
     }
 
+    -- get catppuccin color
+    local catppuccin = require("catppuccin.palettes").get_palette()
+
     require("lualine").setup({
       options = {
         -- no section seperator
@@ -35,28 +38,28 @@ return {
         disabled_filetypes = { "NvimTree", "alpha" },
       },
       sections = {
-        -- stylua: ignore start
+        -- stylua: ignore
         lualine_a = { { "mode", fmt = function(s) return mode_map[s] or s end } },
         lualine_b = { "branch", "diff", "diagnostics" },
         lualine_c = {
-          { "filetype", icon_only = true, separator = "", padding = { right = 0, left = 1 } },
-          { "filename", path = 1 },
+          { "filename", path = 1, color = { fg = catppuccin["text"] } },
         },
+        -- stylua: ignore start
         lualine_x = {
           { "encoding", fmt = function (s) return s ~= "utf-8" and s end },
           { "fileformat", icons_enabled = false, fmt = function (s) return s ~= "unix" and s end },
         },
-        lualine_y = { "location" },
         -- stylua: ignore end
+        lualine_y = { { "filetype", color = { fg = catppuccin["text"] } } },
+        lualine_z = { "location" },
       },
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
         lualine_c = {
-          { "filetype", icon_only = true, separator = "", padding = { right = 0, left = 1 } },
           { "filename", path = 1 },
         },
-        lualine_x = { "location" },
+        lualine_x = { "filetype" },
         lualine_y = {},
         lualine_z = {},
       },
