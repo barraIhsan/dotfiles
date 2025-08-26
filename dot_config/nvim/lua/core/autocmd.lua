e2526419cf2bfbc438eea2a711c989c5f4343a2f
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-08-26 13:32:38 +0700

    refactor(nvim): remove unneeded description

--- dot_config/nvim/lua/core/autocmd.lua
+++ dot_config/nvim/lua/core/autocmd.lua
@@ -1,6 +1,5 @@
 -- highlight when yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
   callback = function()
     vim.highlight.on_yank()
   end,
