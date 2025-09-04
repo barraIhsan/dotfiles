1fcaf3182475654a7438c5825fbc26b129ae3f86
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-09-04 20:42:30 +0700

    feat(nvim): add cursor-like placeholder to snippet jump location
    Refs: https://github.com/MariaSolOs/dotfiles/blob/main/.config/nvim/lua/plugins/luasnip.lua

--- dot_config/nvim/lua/plugins/luasnip.lua
+++ dot_config/nvim/lua/plugins/luasnip.lua
@@ -4,9 +4,29 @@ return {
   build = "make install_jsregexp",
   dependencies = { "rafamadriz/friendly-snippets" },
   config = function()
+    local types = require("luasnip.util.types")
     require("luasnip").config.set_config({
       -- enable autosnippets
       enable_autosnippets = true,
+      -- add cursor-like placeholder to snippet jump location
+      ext_opts = {
+        [types.insertNode] = {
+          visited = {
+            virt_text = { { "|", "Conceal" } },
+            virt_text_pos = "inline",
+          },
+          unvisited = {
+            virt_text = { { "|", "Conceal" } },
+            virt_text_pos = "inline",
+          },
+        },
+        [types.exitNode] = {
+          unvisited = {
+            virt_text = { { "|", "Conceal" } },
+            virt_text_pos = "inline",
+          },
+        },
+      },
     })
 
     -- load vscode-style snippets
