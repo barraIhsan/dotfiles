77dda0dc98f8ff560e3d602e298968fe23fc3895
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2026-01-21 15:21:59 +0700

    fix(nvim,lspkind): icons after the removal of symbolic
    Refs: https://github.com/bezlant/nvim/commit/4b6176b7002228a181712ded744ce0360165489c

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -73,9 +73,7 @@ return {
                     icon = dev_icon
                   end
                 else
-                  icon = require("lspkind").symbolic(ctx.kind, {
-                    mode = "symbol",
-                  })
+                  icon = require("lspkind").symbol_map[ctx.kind] or ""
                 end
                 return icon .. ctx.icon_gap
               end,
