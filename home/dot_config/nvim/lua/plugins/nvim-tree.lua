efd0d0e8aa3a155275714dbd2ce6a99699c4a5ab
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2026-03-29 12:14:46 +0700

    feat(rootmoi): implement rootmoi to track root files
    Using @felipecrs's methods:
    https://github.com/twpayne/chezmoi/discussions/1510#discussioncomment-7808924

--- home/dot_config/nvim/lua/plugins/nvim-tree.lua
+++ home/dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -18,26 +18,27 @@ return {
       view = {
         relativenumber = true,
         number = true,
-        float = {
-          enable = true,
-          open_win_config = function()
-            local screen_w = vim.opt.columns:get()
-            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-            local window_w = screen_w * 0.8
-            local window_h = screen_h * 0.8
-            local window_w_int = math.floor(window_w)
-            local window_h_int = math.floor(window_h)
-            local center_x = (screen_w - window_w) / 2
-            local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
-            return {
-              relative = "editor",
-              row = center_y,
-              col = center_x,
-              width = window_w_int,
-              height = window_h_int,
-            }
-          end,
-        },
+        side = "right",
+        -- float = {
+        --   enable = true,
+        --   open_win_config = function()
+        --     local screen_w = vim.opt.columns:get()
+        --     local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        --     local window_w = screen_w * 0.8
+        --     local window_h = screen_h * 0.8
+        --     local window_w_int = math.floor(window_w)
+        --     local window_h_int = math.floor(window_h)
+        --     local center_x = (screen_w - window_w) / 2
+        --     local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+        --     return {
+        --       relative = "editor",
+        --       row = center_y,
+        --       col = center_x,
+        --       width = window_w_int,
+        --       height = window_h_int,
+        --     }
+        --   end,
+        -- },
       },
       renderer = {
         indent_markers = {
@@ -74,7 +75,7 @@ return {
         git_ignored = false,
       },
       disable_netrw = true,
-      hijack_cursor = true,
+      hijack_cursor = false,
       update_focused_file = {
         enable = true,
         update_root = false,
